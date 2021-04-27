3bbfe259dd97eee74e558aeb47f267178f70954d
LeonardoSeishi <leonardo.seishi@grad.ufsc.br>
2021-04-27 20:58:48 -0300

    nada_de_mais

--- jogador.py
+++ jogador.py
@@ -2,22 +2,22 @@ from objeto import Objeto
 
 class Jogador(Objeto):
     def __init__(self,velocidade, x, y, imagem, largura, altura):
-        super().__init__(imagem, x, y, largura, altura, velocidade = None)
+        super().__init__(imagem, x, y, largura, altura, velocidade)
         self.__pulando = False
         self.__agachado = False
         self.__gravidade = 0.25
 
     def atualizar(self):
-        self.__posicao[1] += self.__velocidade
+        self.__cordenadas[1] += self.__velocidade
 
         if self.__velocidade<= 11.1 and self.__velocidade >= 10.9:
             self.__pulando = False     
-            self.__posicao[1] = 317
+            self.__cordenadas[1] = 317
 
         if self.__pulando:
             self.__velocidade = self.__velocidade + self.__gravidade
             
-    def pular():
+    def pular(self):
         self.__pulando = True
         self.__velocidade = -11
 
