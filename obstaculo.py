45557a4f5f85e849823e996388970dab2f977445
Eduardo Godinho <dudusgodinho@gmail.com>
2021-04-27 10:20:02 -0300

    'classes'

---
+++ obstaculo.py
@@ -0,0 +1,11 @@
+class Obstaculo():
+    def __init__(self,velocidade:int,posicao:list,imagem):
+        self.__velocidade = velocidade
+        self.__posicao = posicao
+        self.__imagem = imagem
+
+    def desenhar(self):
+        pass
+    
+    def atualizar(self):
+        pass
---
+++ obstaculo_movel.py
@@ -0,0 +1,12 @@
+class ObstaculoMovel():
+    def __init__(self,velocidade:int,posicao:list,imagem):
+        self.__velocidade = velocidade
+        self.__posicao = posicao
+        self.__imagem = imagem
+    
+
+    def desenhar(self):
+        pass
+    
+    def atualizar(self):
+        pass
