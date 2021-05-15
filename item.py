db9f4b7db035ce836cb50a358ae7498558ac303f
LeonardoSeishi <71985653+LeonardoSeishi@users.noreply.github.com>
2021-05-14 21:09:40 -0300

    Delete item.py

--- item.py
+++
@@ -1,6 +0,0 @@
-from objeto import Objeto
-
-
-class Item(Objeto):
-    def __init__(self, velocidade, x, y, imagem, largura, altura):
-        super().__init__(velocidade, x, y, imagem, largura, altura)
